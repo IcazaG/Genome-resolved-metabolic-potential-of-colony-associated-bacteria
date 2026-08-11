548e30b0f1f7b79d002b4be7c4ac6cc4397fdae7
IcazaG <114770901+IcazaG@users.noreply.github.com>
2026-08-11 14:36:23 -0400

    Update kofamscan.sh

--- Cluster_script/kofamscan.sh
+++ Cluster_script/kofamscan.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=kofamscan
 #SBATCH --output=kofamscan%j.out
 #SBATCH --error=kofamscan%j.err
-#SBATCH --partition=debug
+#SBATCH --partition=XXXXX
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
